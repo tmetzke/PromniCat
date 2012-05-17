caa2c6b09aa5f9a1a9a3c24d5fefb92b8a388243
Tobias Metzke <tobias.metzke@googlemail.com>
2012-05-17 21:50:03 +0200

    added header

--- src/de/uni_potsdam/hpi/bpt/promnicat/utilityUnits/IFlexibleUnitChainBuilder.java
+++ src/de/uni_potsdam/hpi/bpt/promnicat/utilityUnits/IFlexibleUnitChainBuilder.java
@@ -1,3 +1,20 @@
+/**
+ * PromniCAT - Collection and Analysis of Business Process Models
+ * Copyright (C) 2012 Cindy Fähnrich, Tobias Hoppe, Andrina Mascher
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.uni_potsdam.hpi.bpt.promnicat.utilityUnits;
 
 import java.util.Collection;
@@ -7,6 +24,11 @@ import org.jbpt.pm.ProcessModel;
 import de.uni_potsdam.hpi.bpt.promnicat.util.IllegalTypeException;
 import de.uni_potsdam.hpi.bpt.promnicat.util.ProcessMetricConstants;
 
+/**
+ * 
+ * @author Tobias Metzke
+ *
+ */
 public interface IFlexibleUnitChainBuilder extends IUnitChainBuilder {
 
 	/**
